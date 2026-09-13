36e98c00d4bbd8316a11e196ebda3df6a91c50f0
Shanks0789 <villaceranshandy7@gmail.com>
2026-09-13 14:59:36 +0800

    aaa

--- resources/views/partials/admin-sidebar.blade.php
+++ resources/views/partials/admin-sidebar.blade.php
@@ -12,7 +12,6 @@
             <a class="{{ request()->routeIs('inventory.*') ? 'active' : '' }}" href="{{ route('inventory.index') }}">@include('partials.nav-icon',['name'=>'inventory']) Inventory</a>
             <a class="{{ request()->routeIs('reservations.index') ? 'active' : '' }}" href="{{ route('reservations.index') }}">@include('partials.nav-icon',['name'=>'reservations']) Reservations</a>
             <a class="{{ request()->routeIs('products.*') ? 'active' : '' }}" href="{{ route('products.index') }}">@include('partials.nav-icon',['name'=>'products']) Menu Pictures</a>
-            <a class="{{ request()->routeIs('admins.*') ? 'active' : '' }}" href="{{ route('admins.index') }}">@include('partials.nav-icon',['name'=>'users']) Admin Accounts</a>
             <a class="{{ request()->routeIs('cashiers.*') ? 'active' : '' }}" href="{{ route('cashiers.index') }}">@include('partials.nav-icon',['name'=>'cashier']) Cashier Accounts</a>
             <a class="{{ request()->routeIs('customers.*') ? 'active' : '' }}" href="{{ route('customers.index') }}">@include('partials.nav-icon',['name'=>'users']) Customers</a>
             <a class="{{ request()->routeIs('activity-logs.*') ? 'active' : '' }}" href="{{ route('activity-logs.index') }}">@include('partials.nav-icon',['name'=>'activity']) Activity Logs</a>
