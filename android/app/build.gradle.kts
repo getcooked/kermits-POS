41d7f019d2f9f9e81d7f6301d8324257d96beec2
Shanks0789 <villaceranshandy7@gmail.com>
2026-09-16 21:41:29 +0800

    addre

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -29,8 +29,8 @@ android {
         applicationId = "com.getcooked.kermits"
         minSdk = 26
         targetSdk = 35
-        versionCode = 19
-        versionName = "1.0.18"
+        versionCode = 20
+        versionName = "1.0.19"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         buildConfigField("String", "API_BASE_URL", "\"$productionApiBaseUrl\"")
         buildConfigField("boolean", "FCM_CONFIGURED", firebaseConfigPresent.toString())
